180489e81a97140b338a2c52a927f859b50f7301
thiampo <babacacthiam@gmail.com>
2020-11-01 15:52:22 +0000

    ajoutez cet modification

---
+++ testo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd Bureau
+mkdir mon-premier-test1
+cd mon-premier-test1
+touch edition
+chmod u+x edition
+mkdir -p saved
+cp edition saved// script pour faire des sauvegarde ajoures cet modification 
+
+
